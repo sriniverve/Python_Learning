4da05f5a7b95a7e36807dd5eb1fb28cddb3abbac
Agni <agni.fuoco@gmail.com>
2020-04-30 15:28:48 +0530

    Arithetic Operators

---
+++ venv/tests/arithmetic_operators.py
@@ -0,0 +1,21 @@
+'''
+This is to demonstrate the usage of arithmetic operators
+'''
+
+print(4+10)             #Addition
+print(19-29)            #Subtraction
+print(34*19)            #Multiplication
+print(1452/23)          #Divsion
+print(1452//23)         #Division truncated
+print(145%10)           #Modulo
+print(15**4)            #Exponents
+
+x = 10
+y = 20
+x = x + 3               #Regular assignment after addition
+y += 10                 #Augmented assignment operator
+
+print(x,y)
+
+
+

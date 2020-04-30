56e439bc2cabc42b1e8066d23433f8f4c6f23bde
Agni <agni.fuoco@gmail.com>
2020-04-30 20:02:28 +0530

    lists

---
+++ venv/tests/lists.py
@@ -0,0 +1,8 @@
+'''
+This is to demonstrated the usage of lists
+'''
+
+names = ['Shrenik', 'Nihal', 'Pranav', 'Saira', 'Michelle']
+print("First name in the list is {}".format(names[0]))
+print("The first three names in the list are {}".format(names[:3]))
+

ff8bdfdd4e5026ff239d4e8a503db0fcc721653f
Agni <agni.fuoco@gmail.com>
2020-04-30 19:56:54 +0530

    print the letters F & L using nested for loops

--- venv/tests/nested_loops_to_print_F.py
+++ venv/tests/nested_loops_to_print_F.py
@@ -2,9 +2,16 @@
 This is a program to print the letter F using nested for loops
 '''
 
-numbers = [5, 2, 5, 2, 2]
+f_numbers = [5, 2, 5, 2, 2]
+l_numbers = [2, 2, 2, 2, 5]
 
-for x_count in numbers:
+for x_count in f_numbers:
+    eff = ""
+    for count in range(x_count):
+        eff += 'x'
+    print(eff)
+
+for x_count in l_numbers:
     eff = ""
     for count in range(x_count):
         eff += 'x'

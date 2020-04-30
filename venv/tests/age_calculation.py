16e5d2686097a4ca219c82d22fc1a049d37d5a8c
Agni <agni.fuoco@gmail.com>
2020-04-30 16:29:19 +0530

    Age calculation with variable, expressions & type casting

--- venv/tests/age_calculation.py
+++ venv/tests/age_calculation.py
@@ -2,7 +2,7 @@
 This program is an example to demostrate input variables & expressions
 '''
 
-birth_year = input('What is your year of birth?')
-age = 2020 - int(birth_year)
+birth_year = input('What is your year of birth?')       #Take age input from user
+age = 2020 - int(birth_year)                            #typecasting needed between different data types
 print('Type of age variable is '+str(type(age)))
 print('Your age is '+str(age))

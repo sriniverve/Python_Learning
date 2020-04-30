8ca671f325a57e789539cad55661bc53db137f85
Agni <agni.fuoco@gmail.com>
2020-04-30 16:25:18 +0530

    conditional operators

---
+++ venv/tests/conditions.py
@@ -0,0 +1,12 @@
+'''
+This is to demonstrate the usage of conditions
+'''
+
+temperature = input('Enter the temperature:')
+
+if int(temperature) > 30:
+    print('This is a hot day')
+elif int(temperature) < 5:
+    print('This is a cold day')
+else:
+    print('This is a pleasant day')

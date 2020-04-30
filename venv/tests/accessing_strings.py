f6adf2faa5d0afd4e0fdebb607021c6bd84e970c
Agni <agni.fuoco@gmail.com>
2020-04-30 14:09:29 +0530

    How to access characters from strings

---
+++ venv/tests/accessing_strings.py
@@ -0,0 +1,13 @@
+'''
+This is demostrate how to extract the characters from a string
+'''
+
+course = 'Python for Beginners'
+print(course)
+
+print(course[0])        #This is for the first character
+print(course[-1])       #This is for the last character
+print(course[:])        #To print the entire string
+another = course[1:]    #To print after the first character
+
+print(another)

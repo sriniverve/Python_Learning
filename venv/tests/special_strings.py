99be33e715f15b86d9e8b127d0aefa048451635e
water-acqua <water.acqua20@gmail.com>
2020-04-30 04:30:51 -0400

    new file:   special_strings.py

---
+++ venv/tests/special_strings.py
@@ -0,0 +1,17 @@
+'''
+This program is to demostrate uses of ' & " for printing special strings
+'''
+
+
+course1 = 'Welcome to Python Learning Course'
+course2 = "Welcome to Fuoco's Python Learning Course"
+course3 = 'Welcome to "Aria" special Python Learning Course'
+course4 = '''
+This course print can go into
+multiple lines
+'''
+
+print(course1)
+print(course2)
+print(course3)
+print(course4)

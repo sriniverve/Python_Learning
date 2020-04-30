c394a736dbed0c1e3c90430aee83008b5311cf7f
Agni <agni.fuoco@gmail.com>
2020-04-30 15:38:41 +0530

    Reusable functions (math module)

---
+++ venv/tests/reusable_functions.py
@@ -0,0 +1,17 @@
+'''
+This is to demonstrate reusable functions, we will use built-in math module to use its reusable functions
+'''
+import math
+
+x = 2.9
+y = -24.2
+print(round(x))
+print(abs(y))
+deg = 45
+radians = math.radians(deg)
+print(radians)
+print(math.tan(radians))
+
+
+
+

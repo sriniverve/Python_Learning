1ba79ba198003fcd91b030fe8a48e29120d402e0
Agni <agni.fuoco@gmail.com>
2020-04-30 19:32:06 +0530

    for loops

---
+++ venv/tests/for_loops.py
@@ -0,0 +1,15 @@
+'''
+This is to demonstrate the usage of for loop
+'''
+
+for item in 'Python':
+    print(item)
+
+for item in [1, 2, 3, 4, 5, 6, 7]:
+    print(item)
+
+for item in range(10):
+    print(item)
+
+for item in range(10,101,10):
+    print(item)
---
+++ venv/tests/shopping_cart_total.py
@@ -0,0 +1,8 @@
+'''
+This is an example program to calculate total of the shopping car
+'''
+total = 0
+prices = [1245, 6736, 8728]
+for price in prices:
+    total += price
+print('Total amount is {}'.format(total))

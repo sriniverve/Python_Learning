da9127e94eed9b2e22435921c2c18de2fba481e8
Agni <agni.fuoco@gmail.com>
2020-04-30 16:30:42 +0530

    Input functions

--- venv/tests/input_functions.py
+++ venv/tests/input_functions.py
@@ -2,6 +2,6 @@
 This program is to explain the concept of variables through keyboard input
 '''
 
-name = input('What is your name?')
+name = input('What is your name?')                          #prompting user for input
 fav_color = input('What is your favorite color?')
-print('Hi '+name+' and your favorite color is '+fav_color)
+print('Hi '+name+' and your favorite color is '+fav_color)  #printing with concatenation

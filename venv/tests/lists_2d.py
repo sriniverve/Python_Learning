b2361e30367e1993bca52cd69572fb643dd703d0
Agni <agni.fuoco@gmail.com>
2020-04-30 20:38:04 +0530

    Two dimensional lists

---
+++ venv/tests/lists_2d.py
@@ -0,0 +1,15 @@
+'''
+This is a demonstration of 2dimensional lists
+'''
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matrix[0][2])
+
+for row in matrix:
+    for column in row:
+        print(column)

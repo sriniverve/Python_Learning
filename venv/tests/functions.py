e39e476eac11a8071f8955bbfeb0802796c554fd
Agni <agni.fuoco@gmail.com>
2020-04-30 21:31:46 +0530

    functions

---
+++ venv/tests/functions.py
@@ -0,0 +1,11 @@
+'''
+This is to demonstrate the usage of functions
+'''
+
+def welcomeMessage():
+    print('Hi there!')
+    print('Welcome aboard')
+
+print('Start')
+welcomeMessage()
+print('Finish')

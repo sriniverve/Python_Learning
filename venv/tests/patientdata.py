101d326d183601bbadc6e4874a59b1b46a498b5c
Agni <agni.fuoco@gmail.com>
2020-04-30 16:34:55 +0530

    Example of using variables

--- venv/tests/patientdata.py
+++ venv/tests/patientdata.py
@@ -6,7 +6,7 @@ full_name = 'John Smith'    #Full name of the patient
 age = 20                    #Age of the patient
 is_new = False              #Variable to see if the patient is already existing
 
-print(full_name)
+print(full_name)            #print using variables
 print(age)
 print(is_new)
 

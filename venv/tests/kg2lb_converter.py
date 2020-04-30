485dfdfd319e6cc61319b1fc0b98f0332af65066
Agni <agni.fuoco@gmail.com>
2020-04-30 16:33:07 +0530

    Weight conversion from Kilograms to Pounds

--- venv/tests/kg2lb_converter.py
+++ venv/tests/kg2lb_converter.py
@@ -2,6 +2,6 @@
 This program is to take input in KGs & display the weight in pounds
 '''
 
-weight_kg = input('Enter your weight in Kilograms:')
-weight_lb = 2.2 * float(weight_kg)
+weight_kg = input('Enter your weight in Kilograms:')        #prompt user input
+weight_lb = 2.2 * float(weight_kg)                          #1 kg is 2.2 pounds
 print('You weight is '+str(weight_lb)+' pounds')

0b0818fa5ea475b3b145eef04d7a4b779e0358d8
Agni <agni.fuoco@gmail.com>
2020-04-30 19:34:50 +0530

    shopping cart total using for loops

--- venv/tests/shopping_cart_total.py
+++ venv/tests/shopping_cart_total.py
@@ -1,8 +1,10 @@
 '''
 This is an example program to calculate total of the shopping car
 '''
-total = 0
+
 prices = [1245, 6736, 8728]
+total = 0                           #initialize the variable
+
 for price in prices:
     total += price
 print('Total amount is {}'.format(total))

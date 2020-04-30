ad30fe5eef9b15e7873b4ef35ac6aafa1db36570
Agni <agni.fuoco@gmail.com>
2020-04-30 13:48:10 +0530

    Added new example programs while learning

---
+++ venv/tests/age_calculation.py
@@ -0,0 +1,8 @@
+'''
+This program is an example to demostrate input variables & expressions
+'''
+
+birth_year = input('What is your year of birth?')
+age = 2020 - int(birth_year)
+print('Type of age variable is '+str(type(age)))
+print('Your age is '+str(age))
---
+++ venv/tests/kg2lb_converter.py
@@ -0,0 +1,7 @@
+'''
+This program is to take input in KGs & display the weight in pounds
+'''
+
+weight_kg = input('Enter your weight in Kilograms:')
+weight_lb = 2.2 * float(weight_kg)
+print('You weight is '+str(weight_lb)+' pounds')

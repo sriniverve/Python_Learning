1e11bd5342307f8873b8327202726e9aa19e50a5
Agni <agni.fuoco@gmail.com>
2020-05-01 12:20:46 +0530

    implemented using functions

--- venv/tests/emoji_converter.py
+++ venv/tests/emoji_converter.py
@@ -2,14 +2,17 @@
 This is programs to create shortcuts for emojis
 '''
 
-message = input(">")
-words = message.split(' ')
-emojis = {
-    ":)": "😀",
-    ":(": "☹"
-}
-output = ""
-for word in words:
-    output += emojis.get(word, word)+' '
+def emoji_converter(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "😀",
+        ":(": "☹"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + ' '
+    return output
 
-print(output)
+message = input(">")
+print(emoji_converter(message))
+

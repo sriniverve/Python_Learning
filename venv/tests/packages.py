714c328be5de603e1a9100baa14b5b65da5af4f1
Agni <agni.fuoco@gmail.com>
2020-05-01 19:09:01 +0530

    packages

---
+++ venv/tests/packages.py
@@ -0,0 +1,10 @@
+'''
+This is to demonstrate how packaging is done.
+'''
+
+import ecommerce.shipping
+from ecommerce.shipping import calc_shipping
+
+ecommerce.shipping.calc_shipping()
+
+calc_shipping()

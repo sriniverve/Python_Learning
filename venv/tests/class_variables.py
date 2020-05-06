6fef64fbe39fc524ba8909ddc15d82a53e0979ca
Agni <agni.fuoco@gmail.com>
2020-05-06 11:37:31 +0530

    class variables

---
+++ venv/tests/class_variables.py
@@ -0,0 +1,20 @@
+'''
+This is to demonstrate the usage of class variables
+'''
+
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = f'{first}.{last}@company.com'
+
+    def full_name(self):
+        return f'{self.first} {self.last}'
+
+
+Emp1 = Employee('srini', 'verve', '1000000')
+Emp2 = Employee('shrenik', 'srini', '1500000')
+
+print(Emp2.full_name())

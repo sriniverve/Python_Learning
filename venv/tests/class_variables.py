a876218e46716a0b0a981933663fc6cdb592add5
Agni <agni.fuoco@gmail.com>
2020-05-06 12:09:04 +0530

    class variables with a couple of additions

--- venv/tests/class_variables.py
+++ venv/tests/class_variables.py
@@ -4,17 +4,32 @@ This is to demonstrate the usage of class variables
 
 
 class Employee:
+    raise_amount = 1.04
+    num_of_employees = 0
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = f'{first}.{last}@company.com'
 
+        Employee.num_of_employees += 1
+
     def full_name(self):
         return f'{self.first} {self.last}'
 
+    def apply_raise(self):
+        self.pay = int(float(self.pay) * self.raise_amount)
 
-Emp1 = Employee('srini', 'verve', '1000000')
-Emp2 = Employee('shrenik', 'srini', '1500000')
+emp1 = Employee('srini', 'verve', '1000000')
+emp2 = Employee('shrenik', 'srini', '1500000')
 
-print(Emp2.full_name())
+print(emp1.pay)
+emp1.apply_raise()
+print(emp1.pay)
+
+emp2.raise_amount = 1.05
+emp2.apply_raise()
+print(emp2.pay)
+
+print(Employee.num_of_employees)

9898a8fbe22fb0641525458173818e56c5c9686b
Agni <agni.fuoco@gmail.com>
2020-05-06 11:33:05 +0530

    classes ...added a method

--- venv/tests/classes_example.py
+++ venv/tests/classes_example.py
@@ -9,10 +9,13 @@ class Employee:
         self.pay = pay
         self.email = f'{first}.{last}@company.com'
 
+    def full_name(self):
+        return f'{self.first} {self.last}'
+
 
 Emp1 = Employee('srini', 'verve', '1000000')
 Emp2 = Employee('shrenik', 'srini', '1500000')
 
-print(Emp2.email)
+print(Emp2.full_name())
 
 
